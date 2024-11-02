1985582a8e3c216f8a0cef121bba6f380131e621
Juan Encinas <juan.encinas@upm.es>
2024-11-02 13:11:01 +0100

    Included a new scheduling policy that takes CU decisions.

--- workload_scheduling_simulation.py
+++ workload_scheduling_simulation.py
@@ -40,19 +40,19 @@ class WorkloadSchedulingSimulation:
         self.are_kernels_executable = False  # Flag to check if there are kernels that can be executed (i.e., new arrivals or finished kernels. Set to False to avoid scheduling kernels when waiting queue has been completely checked)
         self.total_scheduling_decisions = 0
         self.active_scheduling_decisions = 0
+        self.tmp_kernel_default_count = 0
         self.cpu_usage = {}
         self.cpu_usage["user"] = random.uniform(30.0, 70.0)
         self.cpu_usage["kernel"] = random.uniform(10.0, 30.0)
         self.cpu_usage["idle"] = 100.0 - self.cpu_usage["user"] - self.cpu_usage["kernel"]
-        self.accessed = 0
-        self.accessed += 1
 
         # Dictionary to map scheduling policies to corresponding methods
         self.scheduling_methods = {
             'FCFS': self._fist_come_first_served_policy,
             'STACK': self._stack_policy,
             'SJF': self._shortest_job_first_policy,
-            'LIF': self._least_interaction_first_policy
+            'LIF': self._least_interaction_first_policy,
+            'CU': self._cu_selection
         }
 
         # Set the scheduling policy
@@ -203,41 +203,6 @@ class WorkloadSchedulingSimulation:
         # Sort the running queue by end time (eases the check of finished kernels)
         self.running_queue.sort(key=lambda x: x['end_time'])
 
-        # print(f"Kernel {kernel['tmp_id']} started at {self.current_time} with {kernel['cu']} CUs")
-        # print(f"Free slots: {self.free_slots}")
-        # print(f"Current configuration: {self.current_configuration}")
-
-    def _execute_kernel_old(self, kernel):
-        """
-        Execute the kernel.
-        """
-
-        # Create feature (TODO: Make CPU usage more realistic)
-        cpu_usage = self.cpu_usage.copy()
-        feature = cpu_usage | self.current_configuration
-        feature["Main"] = kernel["kernel_id"]
-        tmp_kernel_name = self.kernel_names[kernel["kernel_id"]]
-        feature[tmp_kernel_name] = kernel["cu"]
-
-        # Predict the time of execution of the kernel
-        time_prediction = round(self.models[-1].predict_one(feature), 3)
-        # Update the end time of the kernel
-        kernel["end_time"] = self.current_time + kernel["num_executions"] * time_prediction / kernel["cu"]
-        # print(feature)
-        # print(time_prediction)
-        # print(kernel["num_executions"])
-        # print(kernel["end_time"])
-
-        # Move the kernel to the running queue
-        self.running_queue.append(kernel)
-        # Sort the running queue by end time (eases the check of finished kernels)
-        self.running_queue.sort(key=lambda x: x['end_time'])
-        # Remove the kernel from the waiting queue
-        self.waiting_queue.remove(kernel)
-        # Update the free slots
-        self.free_slots -= kernel["cu"]
-        # Update the current configuration
-        self.current_configuration[self.kernel_names[kernel["kernel_id"]]] += kernel["cu"]
         # print(f"Kernel {kernel['tmp_id']} started at {self.current_time} with {kernel['cu']} CUs")
         # print(f"Free slots: {self.free_slots}")
         # print(f"Current configuration: {self.current_configuration}")
@@ -587,6 +552,127 @@ class WorkloadSchedulingSimulation:
 
         return min_kernel
 
+    def _cu_selection(self):
+        """
+        Schedule the kernels in the waiting queue to the running queue.
+        NOTE: Just one scheduling decition is made in each time step.
+        """
+        # TODO: Implement the scheduling algorithm
+
+        num_kernels_to_compare = 2
+        kernels_to_compare = []
+
+        alone_configuration = {"aes": 0, "bulk": 0, "crs": 0, "kmp": 0, "knn": 0, "merge": 0, "nw": 0, "queue": 0, "stencil2d": 0, "stencil3d": 0, "strided": 0}
+
+        # Get the kernels to compare
+        for kernel in self.waiting_queue:
+            # Check if the kernel can be scheduled (i.e., there are free slots available and the kernel has not been scheduled yet(artico3))
+            if self.free_slots >= 2 and self.current_configuration[self.kernel_names[kernel["kernel_id"]]] == 0:
+
+                # Store the kernel to compare
+                kernels_to_compare.append(kernel)
+
+                # Check if there are enough kernels to compare
+                if len(kernels_to_compare) == num_kernels_to_compare: break
+
+            # Check if there are free slots available
+            if self.free_slots == 0:
+                break  # Stop scheduling when no free slots are available
+
+        # Check if there are kernels to compare
+        if len(kernels_to_compare) == 0:
+            # Indicate that there are no kernels that can be executed
+            # Since all the schedulable kernels have been scheduled (wait for arrival of new kernels or finish of running kernels)
+            self.are_kernels_executable = False
+            return None
+
+        # Increase the total scheduling decisions
+        self.total_scheduling_decisions += 1
+
+        # # Return the kernel when there is only one kernel to compare
+        # if len(kernels_to_compare) == 1:
+        #     return kernels_to_compare[0]
+
+        #
+        # Compare the kernels
+        #
+
+        # Initialize the minimum job time and the kernel to be scheduled
+        min_interaction_time = float('inf')
+        min_kernel = None
+        min_cu_option = None
+
+        # print("hello\n")
+
+        for kernel in kernels_to_compare:
+
+            # Set the possible CUs
+            slot_choices = [2, 4]
+            # Get the available CUs
+            cu_available = [slot_choice for slot_choice in slot_choices if slot_choice <= self.free_slots]
+            for cu_option in cu_available:
+
+                # Current configuration
+                # print("current")
+                # Create feature (TODO: Make CPU usage more realistic)
+                cpu_usage = self.cpu_usage.copy()
+                feature = cpu_usage | self.current_configuration
+                tmp_kernel_name = self.kernel_names[kernel["kernel_id"]]
+                feature[tmp_kernel_name] = cu_option
+
+                cu_acumulated_future_execution_time = 0.0
+                cu_acumulated_current_execution_time = 0.0
+                # print("new set of decisions")
+                if not self.running_queue:
+                    # TODO: solve this
+                    kernel["cu"] = cu_available[-1]
+                    self.tmp_kernel_default_count += 1
+                    return kernel # Schedule first kernel if no kernels are running # TODO: Remove
+
+                for in_execution_kernel in self.running_queue:
+
+                    feature["Main"] = in_execution_kernel["kernel_id"]
+
+                    # Predict the time of execution of the kernel
+                    future_predicted_time = round(self.models[-1].predict_one(feature), 3)
+
+                    tmp_job_performed_since_last_update = (self.current_time - in_execution_kernel["last_update_time"]) / (in_execution_kernel["end_time"] - in_execution_kernel["last_update_time"])
+                    future_job_remaining_percentage = in_execution_kernel["job_remaining_percentage"] * (1 - tmp_job_performed_since_last_update)
+
+                    future_execution_time = future_predicted_time * in_execution_kernel["num_executions"] * future_job_remaining_percentage / in_execution_kernel["cu"]
+                    # print("Kernel: ", in_execution_kernel["tmp_id"])
+                    # print(feature)
+                    # print(future_predicted_time)
+                    # print(future_execution_time)
+
+                    current_execution_time = in_execution_kernel["last_predicted_time"] * in_execution_kernel["num_executions"] * future_job_remaining_percentage / in_execution_kernel["cu"]
+
+                    # print("current: ", current_execution_time)
+                    # print("future: ", future_execution_time)
+
+                    cu_acumulated_current_execution_time += current_execution_time
+                    cu_acumulated_future_execution_time += future_execution_time
+
+                cu_acumulated_interaction_time = (cu_acumulated_future_execution_time - cu_acumulated_current_execution_time) / cu_acumulated_current_execution_time
+
+                # Update min cu option
+                min_cu_option = cu_option if cu_acumulated_interaction_time < min_interaction_time else min_cu_option
+                # Update the min kernel
+                min_kernel = kernel if cu_acumulated_interaction_time < min_interaction_time else min_kernel
+                # Check if the job time is the minimum
+                min_interaction_time = min(cu_acumulated_interaction_time, min_interaction_time)
+
+                # print("acumulated_current_execution_time", acumulated_current_execution_time)
+
+        # Increase the active scheduling decisions
+        if kernels_to_compare[0] != min_kernel or kernels_to_compare[0]["cu"] != min_cu_option:
+            self.active_scheduling_decisions += 1
+            # print("Decided to change")
+
+        # print("Min kernel: ", min_kernel["tmp_id"])
+
+        return min_kernel
+
 
     def run(self):
         """
@@ -636,6 +722,8 @@ class WorkloadSchedulingSimulation:
         # print(f"Finish time: {finish}")
         # print(f"Schedule time: {schedule}")
 
+        print("self.tmp_kernel_default_count", self.tmp_kernel_default_count)
+
         total_wait_time = 0.0
         for kernel in self.finished_queue:
             total_wait_time += kernel["start_time"] - kernel["arrival_ms"]
@@ -656,7 +744,7 @@ def generate_workload(workload_information_dict, board):
     """
 
     # Define the number of slots available in the board
-    slot_choices = [1, 2, 4, 8] if board == "ZCU" else [1, 2, 4]
+    slot_choices = [1,2,4,8] if board == "ZCU" else [1, 2, 4]
 
     workload = []
     arrival_time = 0.0
@@ -813,9 +901,9 @@ def main():
     # Scheduler Initialization
     #
 
-    sim_0 = WorkloadSchedulingSimulation(online_models_list, workload_0, "FCFS", "ZCU")
-    sim_1 = WorkloadSchedulingSimulation(online_models_list, workload_1, "FCFS", "ZCU")
-    sim_2 = WorkloadSchedulingSimulation(online_models_list, workload_2, "FCFS", "ZCU")
+    sim_0 = WorkloadSchedulingSimulation(online_models_list, workload_0, "CU", "ZCU")
+    sim_1 = WorkloadSchedulingSimulation(online_models_list, workload_1, "CU", "ZCU")
+    sim_2 = WorkloadSchedulingSimulation(online_models_list, workload_2, "CU", "ZCU")
 
     simulations = [sim_0, sim_1, sim_2]
 
